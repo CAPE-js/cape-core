7ebfab5a2a3aa588e43f07bd78007b468ee0c38b
totl <totl@soton.ac.uk>
2022-06-16 15:25:43 +0100

    don't minify html

--- vue.config.js
+++ vue.config.js
@@ -14,6 +14,10 @@ module.exports = defineConfig({
     output: {
       filename: 'main.js',
       path: path.resolve(__dirname, 'dist'),
-    },
+    }
+  },
+  chainWebpack: config => {
+    // set HTML to not minify
+    config.plugin('html').tap( options => { options[0].minify=false; return options; } );
   }
 })
