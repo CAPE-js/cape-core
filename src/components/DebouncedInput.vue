345680058907fea0c1c22eec9b0c178bc2c0a9e9
cgutteridge <christophergutteridge@gmail.com>
2022-06-23 22:36:04 +0100

    updated debounce

--- src/components/DebouncedInput.vue
+++ src/components/DebouncedInput.vue
@@ -1,45 +1,28 @@
 
 
 <template>
-  <input
-    :id="input_id"
-    :placeholder="input_placeholder"
-    :type="input_type"
-    :value="input_value"
-    @input="debounce_input"
-  >
+  <input @input="debounceInput">
 </template>
 
 <script>
-import { debounce } from "lodash"
 export default {
     name: "DebouncedInput", 
-    props: { 
-        type: { type: String, default: "text" } , 
-        value:{ type: [String,Number], default: "" }, 
-        id:   { type: String, default: null },
-        placeholder: { type:String, default: ""}
-    },
-    // note: use computed to expose props as local values, this avoids bad practice vue warning.
-    computed: {
-        input_type: function() {
-            return this.type;
-        },
-        input_value: function() {
-            return this.value;
-        },
-        input_id: function() {
-            return this.id;
-        },
-        input_placeholder: function() {
-            return this.placeholder;
+    props: { modelValue: { type: String, default: "" } },
+    emits: [ "update:modelValue" ],
+    data: () => ({
+        debounce: null
+    }),
+    methods: {
+        debounceInput(event) {
+            this.message = null
+            this.typing = 'You are typing'
+            clearTimeout(this.debounce)
+            this.debounce = setTimeout(() => {
+                this.typing = null
+                this.message = event.target.value
+            this.$emit('update:modelValue', event.target.value);
+            }, 100)
         }
-    },
-    created: function() {
-        var this_component = this;
-        this.debounce_input= debounce(function (e) {
-            this_component.$emit('input', e.target.value);
-        }, 500);
     }
 }
 </script>
