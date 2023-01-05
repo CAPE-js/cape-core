1a8915487d115efda2afadbeb9009cce999477e4
David Pepper <dp11g14@soton.ac.uk>
2023-01-05 11:14:58 +0000

    Adds UI tests for details page.

---
+++ e2e/pageObjects/detailsPageObject.js
@@ -0,0 +1,65 @@
+import { buildRecordUrl } from "../testSiteUrls";
+import { BasePageObject } from "./basePageObject";
+
+export class DetailsPageObject extends BasePageObject {
+
+    constructor(page) {
+        super(page);
+
+        this.nextLink = page.getByRole('link').filter({ hasText: 'Next ➡️' });
+        this.previousLink = page.getByRole('link').filter({ hasText: '⬅️ Previous' });
+    }
+
+    async goto(recordNumber) {
+        const address = buildRecordUrl(recordNumber);
+        await this.page.goto(address);
+    }
+
+    async getCardTitleText() {
+        return await this.page.locator(".card-title").first().innerText();
+    }
+
+    async getFieldValueText(index) {
+        return await this.page.locator(".field-value").nth(index).innerText();
+    }
+
+    async getRecordNumberText() {
+        return await this.getFieldValueText(0);
+    }
+
+    async getDateText() {
+        return await this.getFieldValueText(1);
+    }
+
+    async getTitleText() {
+        return await this.getFieldValueText(2);
+    }
+
+    async getSizeText() {
+        return await this.getFieldValueText(3);
+    }
+
+    async getColourText() {
+        return await this.getFieldValueText(4);
+    }
+
+    async getThreeCharColourText() {
+        return await this.getFieldValueText(5);        
+    }
+
+    async getWeightText() {
+        return await this.getFieldValueText(6);
+    }
+
+    async getLikesText() {
+        return await this.getFieldValueText(7);
+    }
+
+    async getFoodText() {
+        return await this.getFieldValueText(8);
+    }
+
+    async getDrinksText() {
+        return await this.getFieldValueText(9);
+    }
+}
--- e2e/testSiteUrls.js
+++ e2e/testSiteUrls.js
@@ -3,3 +3,7 @@ export const rootUrl = "http://localhost:8080/#/"
 export function buildPath(segments) {
     return rootUrl + segments;
 }
+
+export function buildRecordUrl(recordNumber) {
+    return rootUrl + `record/${recordNumber}`;
+}
