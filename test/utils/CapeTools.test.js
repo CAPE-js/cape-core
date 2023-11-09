f0584410f1ab8a92d0364894a85f32b3f59b4dc3
Christopher Gutteridge <totl@soton.ac.uk>
2023-11-09 12:48:40 +0000

    more lint

--- test/utils/CapeTools.test.js
+++ test/utils/CapeTools.test.js
@@ -1,74 +1,71 @@
-import { CapeTools } from '../../src/utils/CapeTools';
+import { CapeTools } from '../../src/utils/CapeTools'
 import { expect, test } from 'vitest'
 
 describe('CapeTools', () => {
+  describe('make_pattern', () => {
+    test('returns a regular expression which matches when strings differ only by diacritics', () => {
+      const input = "Who doesn't like crème brûlée?"
+      const inputWithoutAccents = "Who doesn't like creme brulee?"
 
-    describe('make_pattern', () => {
-        
-        test('returns a regular expression which matches when strings differ only by diacritics', () => {
-            const input = "Who doesn't like crème brûlée?";
-            const inputWithoutAccents = "Who doesn't like creme brulee?";            
+      const regex = CapeTools.make_pattern(inputWithoutAccents)
 
-            const regex = CapeTools.make_pattern(inputWithoutAccents);
+      const result = input.match(regex)
 
-            const result = input.match(regex);
+      expect(result).not.toBeNull()
+    })
 
-            expect(result).not.toBeNull();
-        });
+    test('returns a regular expression which matches when strings differ only by case', () => {
+      const input = 'I am the Walrus'
+      const lowercaseInput = input.toLocaleLowerCase()
 
-        test('returns a regular expression which matches when strings differ only by case', () => {
-            const input = "I am the Walrus";        
-            const lowercaseInput = input.toLocaleLowerCase();
+      const regex = CapeTools.make_pattern(lowercaseInput)
 
-            const regex = CapeTools.make_pattern(lowercaseInput);
+      const result = input.match(regex)
 
-            const result = input.match(regex);
+      expect(result).not.toBeNull()
+    })
 
-            expect(result).not.toBeNull();
-        });
-    
-        test('returns a regular expression which matches when strings are the same', () => {
-            const input = "I am the Walrus";        
-            
-            const regex = CapeTools.make_pattern(input);
+    test('returns a regular expression which matches when strings are the same', () => {
+      const input = 'I am the Walrus'
 
-            const result = input.match(regex);
-            
-            expect(result).not.toBeNull();
-        });
+      const regex = CapeTools.make_pattern(input)
 
-        test('returns a regular expression which does not matche when strings are not the same', () => {
-            const input = "Crème brûlée is not a Walrus";        
-            const differingText = "This is some differing text";
+      const result = input.match(regex)
 
-            const regex = CapeTools.make_pattern(differingText);
-            
-            const result = input.match(regex);
-            
-            expect(result).toBeNull();
-        });
+      expect(result).not.toBeNull()
+    })
 
-        test('returns a regular expression which matches a single word in a string', () => {
-            const phrase = "The quick brown fox jumps over the lazy dog";
-            const searchTerm = "brown";
+    test('returns a regular expression which does not matche when strings are not the same', () => {
+      const input = 'Crème brûlée is not a Walrus'
+      const differingText = 'This is some differing text'
 
-            const regex = CapeTools.make_pattern(searchTerm);
+      const regex = CapeTools.make_pattern(differingText)
 
-            const result = phrase.match(regex);
+      const result = input.match(regex)
 
-            expect(result).not.toBeNull();
-        });
+      expect(result).toBeNull()
+    })
 
-        test('returns a regular expression which matches a part of a word in a string', () => {
-            const phrase = "The quick brown fox jumps over the lazy dog";
-            const searchTerm = "azy";
+    test('returns a regular expression which matches a single word in a string', () => {
+      const phrase = 'The quick brown fox jumps over the lazy dog'
+      const searchTerm = 'brown'
 
-            const regex = CapeTools.make_pattern(searchTerm);
+      const regex = CapeTools.make_pattern(searchTerm)
 
-            const result = phrase.match(regex);
+      const result = phrase.match(regex)
 
-            expect(result).not.toBeNull();
-        });
-    });   
-});
+      expect(result).not.toBeNull()
+    })
 
+    test('returns a regular expression which matches a part of a word in a string', () => {
+      const phrase = 'The quick brown fox jumps over the lazy dog'
+      const searchTerm = 'azy'
+
+      const regex = CapeTools.make_pattern(searchTerm)
+
+      const result = phrase.match(regex)
+
+      expect(result).not.toBeNull()
+    })
+  })
+})
